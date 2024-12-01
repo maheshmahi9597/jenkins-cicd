b0ba51102149476b32fdba96c6c637d526a518f2
maheshmahi9597 <151617065+maheshmahi9597@users.noreply.github.com>
2024-12-01 16:40:04 +0530

    Updated once app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello mahesh and  Devops Intern Team "
+    return "Hello rajkumar eeeee and  Devops Intern Team "
 
 if __name__ == "__main__":
     app.run(debug=True)
