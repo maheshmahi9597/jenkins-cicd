12085f19086d8fa313e3f85002b055ba0c2016d0
maheshmahi9597 <151617065+maheshmahi9597@users.noreply.github.com>
2025-04-05 15:55:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello Devops Intern Team "
+    return "Hello TEchno Hacks Team this is erajala mahesh demo1 "
 
 if __name__ == "__main__":
     app.run(debug=True)
